05c4c5cbe4f88cddee46112ddb4ebf7718e6c8dc
Leo Riviera <leo.riviera@voxa.dev>
2022-05-22 13:13:59 +0100

    Add parse and lex modes to REPL

--- src/repl.ts
+++ src/repl.ts
@@ -1,14 +1,32 @@
 import repl from 'node:repl';
+import util from 'node:util';
 
 import { Lexer } from './lexer';
+import { Parser } from './parser';
 import { keywords } from './tokens';
+
+const { argv } = process;
+
+const parseMode = argv.includes('-p');
+const lexMode = argv.includes('-l');
+
+const mode = lexMode ? 'lex' : parseMode ? 'parse' : 'parse';
 
 repl.start({
     eval: (input, _context, _filename, callback) => {
-        const l = new Lexer(input);
-        const tokens = l.lexInput();
-
-        callback(null, tokens);
+        switch (mode) {
+            case 'lex': {
+                const l = new Lexer(input);
+                callback(null, l.lexInput());
+                break;
+            }
+            case 'parse': {
+                const p = new Parser(input);
+                const program = p.parseProgram();
+                callback(null, program);
+                break;
+            }
+        }
     },
     completer: (line: string, callback) => {
         const completed = Object.keys(keywords).filter((keyword) =>
@@ -17,4 +35,9 @@ repl.start({
 
         callback(null, [completed, line]);
     },
+    writer: (output: string) =>
+        util.inspect(output, {
+            depth: Infinity,
+            colors: true,
+        }),
 });
